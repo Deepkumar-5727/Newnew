f3557573162bc3c47729f4af109c1e674af30f09
Deepkumar-5727 <deep_kumar@myyahoo.com>
2025-08-31 17:09:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1415,7 +1415,7 @@ async def txt_handler(bot: Client, m: Message):
          
             elif "https://cpvod.testbook.com/" in url or "classplusapp.com/drm/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={15389632871}"
+                url = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={5389632871}"
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
